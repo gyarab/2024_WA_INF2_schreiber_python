8c224072bbfcdf695cc69e40abf5fb616b960548
Schreiber-gyarab <117259092+Schreiber-gyarab@users.noreply.github.com>
2024-12-12 09:35:11 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
-
+def fibonacci(n):
+    if not isinstance(n, int):
+        raise ValueError("n must be an integer") or n < 0
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+    
+if __name__ == "__main__":
+    print(fibonacci(10))
