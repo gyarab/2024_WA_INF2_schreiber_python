56c678aa2f249c22e4303d0e5f89d4e613ee99a4
Schreiber-gyarab <117259092+Schreiber-gyarab@users.noreply.github.com>
2024-12-12 09:47:02 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 def fibonacci(n):
-    if not isinstance(n, int) or n < 0:
-        raise ValueError("n must be an integer")
+    if not isinstance(n, int):
+        raise ValueError("n must be an integer") or n < 0
     if n == 0:
         return 0
     elif n == 1:
@@ -8,5 +8,17 @@ def fibonacci(n):
     else:
         return fibonacci(n-1) + fibonacci(n-2)
     
+def is_prime(number) -> bool:
+    if not isinstance(number, int) or number < 0:
+        raise ValueError("number must be a non-negative integer")
+    if number < 2:
+        return False
+    for i in range(2, number):
+        if number % i == 0:
+            return False
+    return True
+
+
 if __name__ == "__main__":
-    print(fibonacci(10))
+    print(fibonacci(10)) 
+    print(is_prime(10))
